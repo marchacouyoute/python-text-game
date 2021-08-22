4eaa52bbb02702365c7efe6e987cfe2b54acc573
Marcha Couyoute <marchacouyoute1@gmail.com>
2021-08-22 14:14:50 -0400

    Added python file

---
+++ text-game.py
@@ -0,0 +1,8 @@
+rightAnswer = 'Correct'
+wrongAnswer = 'Incorrect'
+
+questionAnswer1 = input('Is the sky blue?: ')
+if questionAnswer1 == 'yes':
+    print(rightAnswer)
+else:
+    print(wrongAnswer)
